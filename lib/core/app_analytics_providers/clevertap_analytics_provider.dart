9523a9e18bc5534458d63a4b10cca371d8102979
Ismail A. Amassi <32588971+IsmailAAmassi@users.noreply.github.com>
2023-06-19 01:16:09 +0300

    implement custom attribute for users in _sendCustomAttributes

--- lib/core/app_analytics_providers/clevertap_analytics_provider.dart
+++ lib/core/app_analytics_providers/clevertap_analytics_provider.dart
@@ -1,6 +1,7 @@
 import 'package:bond/core/app_analytics.dart';
 import 'package:bond_core/core.dart';
 import 'package:clevertap_plugin/clevertap_plugin.dart';
+import 'package:flutter/cupertino.dart';
 
 class CleverTapAnalyticsProvider extends AnalyticsProvider {
   @override
@@ -15,7 +16,7 @@ class CleverTapAnalyticsProvider extends AnalyticsProvider {
   void setUserAttributes(Map<String, dynamic> attributes) {
     // TODO mapping with CleverTap Standers Attribute Names
     // Example of Attribute Names Identity instead of id or user_id, DOB (dd-MM-yyyy) use CleverTapPlugin.getCleverTapDate(new DateTime.now())
-    CleverTapPlugin.profileSet(attributes);
+    attributes.forEach(_sendCustomAttributes);
   }
 
   @override
@@ -26,4 +27,27 @@ class CleverTapAnalyticsProvider extends AnalyticsProvider {
     });
   }
 
+  void _sendCustomAttributes(String key, dynamic value) {
+    var userPropertyKey = key.toLowerCase().replaceAll(' ', '_');
+    debugPrint(
+      'CleverTapAnalyticsProvider send custom user attribute with key: '
+      '$userPropertyKey, '
+      'value: $value and value type: ${value.runtimeType}',
+    );
+    if (value is DateTime) {
+      final dobDate = CleverTapPlugin.getCleverTapDate(value);
+      CleverTapPlugin.profileSet({
+        userPropertyKey: dobDate,
+      });
+    } else if (value is String && (value == 'id' || value == 'user_id')) {
+      CleverTapPlugin.profileSet({
+        'Identity': value,
+        'id': value,
+      });
+    } else {
+      CleverTapPlugin.profileSet({
+        userPropertyKey: value,
+      });
+    }
+  }
 }
