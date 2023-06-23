6f3e6550eef0632b4b0a4e56ce13227bb9524463
Ismail A. Amassi <32588971+IsmailAAmassi@users.noreply.github.com>
2023-06-23 22:23:47 +0300

    continue android integration for android version 12 or above

--- android/app/src/main/kotlin/ps/app/bond/MainActivity.kt
+++ android/app/src/main/kotlin/ps/app/bond/MainActivity.kt
@@ -1,6 +1,46 @@
 package ps.app.bond
 
 import io.flutter.embedding.android.FlutterActivity
+import com.clevertap.android.sdk.*
+import android.content.Intent
+import android.os.Build
+import android.app.NotificationManager
 
-class MainActivity: FlutterActivity() {
+import android.os.Bundle
+
+class MainActivity : FlutterActivity(), PushPermissionResponseListener {
+    var cleverTapDefaultInstance: CleverTapAPI? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        cleverTapDefaultInstance = CleverTapAPI.getDefaultInstance(this)
+        cleverTapDefaultInstance?.registerPushPermissionNotificationResponseListener(this)
+    }
+
+    override fun onPushPermissionResponse(accepted: Boolean) {
+        if (accepted) {
+            CleverTapAPI.createNotificationChannel(
+                    getApplicationContext(),
+                    "famcare_ct",
+                    "Famcare Marketing",
+                    "Famcare Marketing Channel for CleverTap",
+                    NotificationManager.IMPORTANCE_HIGH,
+                    true
+            );
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cleverTapDefaultInstance?.unregisterPushPermissionNotificationResponseListener(this)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        // On Android 12, Raise notification clicked event when Activity is already running in activity backstack
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            cleverTapDefaultInstance?.pushNotificationClickedEvent(intent!!.extras)
+        }
+    }
 }
